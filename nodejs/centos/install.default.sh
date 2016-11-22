6a0ecabb3929ccd50baa1b7158e3beb4185238ac
Carlos De Dios <c.dedios@outlook.com>
2016-11-22 15:35:38 +0000

    Commenting NodeJs Scripts

--- nodejs/centos/install.default.sh
+++ nodejs/centos/install.default.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
+# description: A basic NodeJs installation
+# operating_systems: CentOS
+# tags: NodeJs,CentOS
+# packages: NodeJs,Git
+# created: 11/21/2016
+# last_updated: 11/21/2016
+# variables: repository, host, IP, PORT
+# $repository: The Github repository
+# $host: The hostname
+# $IP: The host preceded with 'http'
+# $PORT: The port the node application will run
 
-## Variables
+####################
+## USER VARIABLES ##
+####################
+repository='http://github.com/droppanel/nodejs.git'
 host=`hostname -I | awk '{print $1}'`
 IP="http://$host"
 PORT=8080
 
+
+#######################
+## Installing NodeJS ##
+#######################
 echo "# Adding ERPL for NodeJs"
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 
 echo "# Installing nodejs"
 yum -y install nodejs
 
+
+###################################
+## Installing And Setting Up Git ##
+###################################
 echo "# Installing Git"
 yum -y install git
 
 echo "# Clonning repository"
-git clone http://github.com/droppanel/nodejs.git app
+git clone $repository app
 
 echo "# Enter repo folder"
 cd app
 
+
+#############################
+## Installing NPM Packages ##
+#############################
 echo "# Install npm packages"
 npm install
 
@@ -27,13 +53,18 @@ npm install
 echo "# Installing PM2"
 npm install pm2@latest -g
 
-# Setting ip tables to redirect from port $PORT to port 80
+
+##############################################
+## Redirecting trafic from $PORT to port 80 ##
+##############################################
 echo "# Setting port $PORT to port 80"
 iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port $PORT
-
-# Updating iptables changes
 iptables-save > /etc/sysconfig/iptables
 
+
+####################
+## Running Server ##
+####################
 echo "# Running node app"
 pm2 start server.js
--- nodejs/ubuntu/install.default.sh
+++ nodejs/ubuntu/install.default.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
+# description: A basic NodeJs installation
+# operating_systems: Ubuntu
+# tags: NodeJs,Ubuntu
+# packages: NodeJs,Git
+# created: 11/21/2016
+# last_updated: 11/21/2016
+# variables: repository, host, IP, PORT
+# $repository: The Github repository
+# $host: The hostname
+# $IP: The host preceded with 'http'
+# $PORT: The port the node application will run
 
-## Variables
+####################
+## USER VARIABLES ##
+####################
+repository='http://github.com/droppanel/nodejs.git'
 host=`hostname -I | awk '{print $1}'`
 IP="http://$host"
 PORT=8080
 
+
+#######################
+## Installing NodeJS ##
+#######################
 echo "# Getting Node source"
 cd ~
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
@@ -18,15 +36,23 @@ sudo apt-get install nodejs
 echo "# Linking nodejs to node command"
 sudo ln -s `which nodejs` /usr/bin/node
 
+
+###################################
+## Installing And Setting Up Git ##
+###################################
 echo "# Installing git"
 sudo apt-get install git
 
 echo "# Clone repository"
-git clone http://github.com/droppanel/nodejs.git app
+git clone $repository app
 
 echo "# Enter repo folder"
 cd app
 
+
+#############################
+## Installing NPM Packages ##
+#############################
 echo "# Install npm packages"
 npm install
 
@@ -34,6 +60,10 @@ npm install
 echo "# Installing PM2"
 sudo npm install pm2@latest -g
 
+
+##############################################
+## Redirecting trafic from $PORT to port 80 ##
+##############################################
 # Commenting exit on rc.local so we can set the iptables prerouting
 echo "# Commenting exit on rc.local"
 sudo sed -i '14 s/^/#/ ' /etc/rc.local  
@@ -46,5 +76,9 @@ sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-po
 # Updating /etc/rc.local to add the iptables change
 echo "iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port $PORT" >> /etc/rc.local
 
+
+####################
+## Running Server ##
+####################
 echo "# Running node app"
 pm2 start server.js
